f913c2c45a5a59fac3ada1886cbd5a9cb04fded4
Ritunjaya Saxena <137945325+ritunjaya@users.noreply.github.com>
2024-05-05 20:54:44 +0530

    build

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,3 +1,3 @@
 
-export const BASE_URL = 'http://localhost:5000';
+export const BASE_URL = 'https://wasserstoff-backendtask-35f0.onrender.com';
 export const BASE_URL_CLOUDINARY = "https://res.cloudinary.com/dwhyyymds/image/upload";
